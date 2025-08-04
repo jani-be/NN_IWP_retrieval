e5d42e9c5b4c713c8f8cdfcea2eec6487df40ea7
Janina Boemeke <janina.boemeke@gmail.com>
2025-08-04 14:16:35 +0200

    updated flight levels and pamtra hamp BTs comparison

--- NN_training_and_development/Comparison_halo_pamtra.py
+++ NN_training_and_development/Comparison_halo_pamtra.py
@@ -22,6 +22,10 @@ sys.path.append('/home/u/u301032/orcestra/NN_IWP_retrieval/')
 #import src
 import src_comparison_halo_pamtra as chp
 
+import matplotlib.colors as colors
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
 #%% Data Comparison condensate loads all campaign days with reruns (2D field)
 #Read in all 2D fields.
 '''
@@ -31,9 +35,9 @@ plus choosing of flight levels
 
 #ds_remap.coarsen(lat=1,lon=1,boundary='pad').construct({"lat":("lat_c",lat), "lon":("lon_c",lon)})
 
+ds_halo,ds_halo_altitude,ds_halo_iwv_KW,ds_sondes = chp.reading_halo_data()
 
-
-
+print(ds_halo)
 
 #%% Data Comparison condensate loads all campaign days with reruns (2D field)
 #Read in all 2D fields.
@@ -142,8 +146,7 @@ si_units = []
 ds_bins_alldays = xr.Dataset()
 #xarray Grundgerüst erstellen  
 bins_all= pd.read_csv("hyd_climatology_n.csv")
-for variable in variabl
-es:
+for variable in variables:
     bin_values = bins_all["bins_"+variable] # unterschiedlich für jede Größe
     time_values=pd.date_range(start='2024-08-10', end='2024-09-30')
     array =  np.zeros((len(time_values),len(bin_values))) #n2 # shape: time,bins
@@ -246,6 +249,9 @@ def old_hist():
 
 
 #%% Reading halo data
+def only_tropics(ds):
+  ds=ds.sel(time=slice('2024-08-01','2024-10-01'))
+  return ds
 def Reading_halo_data():
   DATE ="0829"
 
@@ -254,17 +260,161 @@ def Reading_halo_data():
   file_flight_0829="ipns://latest.orcestra-campaign.org/products/HALO/radiometer/HALO-20240829a.zarr"
   file_altitude = 'ipns://latest.orcestra-campaign.org/products/HALO/position_attitude/HALO-20240829a.zarr'
 
-  ds_halo=xr.open_dataset(file_flight_0829,engine="zarr")
+  #ds_halo=xr.open_dataset(file_flight_0829,engine="zarr")
+  ds_halo=xr.open_dataset("ipfs://bafybeicbj76n3hi52pxtcyzu5in7efk36fk7lavauishclybrsbvlrpq3e", engine="zarr")#radiometer
+  #ds_halo_altitude = xr.open_dataset(file_altitude,engine ="zarr")
+  ds_halo_altitude = xr.open_dataset("ipfs://bafybeias3h5uxtt4ky4d4gn6l6gxjqfkzbde5jlunya6g3umnkvn7xoyoe", engine="zarr") #altitude
+  ds_halo_iwv_KW=xr.open_dataset("ipfs://bafybeicahqvp4lovuqpu63euo5kbc22sdq4jp5p6h6wib373x72ki34tiu", engine="zarr")#IWV from KW band
+  #ds_halo_iwv_KW=xr.open_dataset("ipns://latest.orcestra-campaign.org/products/HALO/iwv/HALO-20240829a.zarr",
+  #                engine="zarr")
+  ds_sondes = xr.open_dataset("ipfs://bafybeicb33v6ohezyhgq5rumq4g7ejnfqxzcpuzd4i2fxnlos5d7ebmi3m", engine="zarr")#dropsondes
+  ds_radar = xr.open_dataset("ipfs://bafybeigmd3dovwm45ylfqxnn2jphsrdjl2jt3dfytv7grkyhleaq42jthe", engine="zarr") #MIRA Cloud Radar Moments
 
-  ds_halo_altitude = xr.open_dataset(file_altitude,engine ="zarr")
-  ds_halo_iwv=xr.open_dataset("ipns://latest.orcestra-campaign.org/products/HALO/iwv/HALO-20240829a.zarr",
-                  engine="zarr")
-  return ds_halo,ds_halo_altitude,ds_halo_iwv
+  return only_tropics(ds_radar),only_tropics(ds_halo),only_tropics(ds_halo_altitude),only_tropics(ds_halo_iwv_KW),ds_sondes
 
-#Reading_halo_data()
+
+ds_radar,ds_halo,ds_halo_altitude,ds_halo_iwv_KW,ds_sondes = Reading_halo_data()
+#%%
+# selecting only one day:
+# Define the target date
+target_date ='2024-08-11'# '2024-09-16'#'2024-09-19'#'2024-09-26'#'2024-08-11'
+def sel_target_date(target_date,ds):
+  target_datetime = pd.to_datetime(target_date)
+  if 'time' in ds.coords:
+     ds_filtered=ds.sel(time=slice(target_datetime, target_datetime + pd.Timedelta(days=1)))
+  elif 'sonde_time' in ds.coords:
+    ds_filtered = ds.where(ds['sonde_time'].dt.date == xr.DataArray(pd.to_datetime(target_date).date()), drop=True)
+  else:
+    print("please check dimension names for time dimension")
+  return ds_filtered
+ 
+ds_sondes_f=sel_target_date(target_date,ds_sondes)
+ 
+#ds_sondes_f = ds_sondes.where(ds_sondes['sonde_time'].dt.date == xr.DataArray(pd.to_datetime(target_date).date()), drop=True)
+
+ds_halo_iwv_KW_f=sel_target_date(target_date,ds_halo_iwv_KW)
+#ds_halo_iwv_KW_f=ds_halo_iwv_KW.sel(time=slice(target_datetime, target_datetime + pd.Timedelta(days=1)))
+ds_halo_altitude_f=sel_target_date(target_date,ds_halo_altitude)
+# Now filtered_ds_sondes contains only the entries for August 9, 2024
+print(ds_sondes_f)
+ds_halo_iwv_KW_f.IWV.plot()
+plt.plot(ds_sondes_f.sonde_time,ds_sondes_f.iwv,'.')
+plt.show()
+#ds_halo_altitude.alt.sel(time=slice(target_datetime, target_datetime + pd.Timedelta(days=1))).plot()
+plt.show()
+#%%
+#plt.plot(ds_sondes.sel(sonde_time=time_straigt).sonde_time,ds_sondes.sel(sonde_time=time_straigt).iwv,'.')
+#plt.show()
+ds_halo_altitude.alt.sel(time=time_straight).plot()
+plt.show()
+#%% PLOT 3D DATAof retrieval
+#open datasets including new iwv
+ds_hamp=xr.open_dataset('/work/um0203/u301032/master_thesis/retrieved_data/HAMP_with_IWV_IWP_LWP_v1-1.nc')
+#ds_iwv_kw=xr.open_dataset("/work/um0203/u301032/master_thesis/flight_data/halo_iwv_kw.nc")
+ds_sondes=xr.open_dataset("/work/um0203/u301032/master_thesis/flight_data/halo_sondes.nc")
+ds_halo_altitude=xr.open_dataset("/work/um0203/u301032/master_thesis/flight_data/halo_altitude.nc") 
+#%% histograms sondes vs halo
+plt.hist([ds_sondes.iwv,ds_hamp.IWV],label=['dropsondes','HAMP'],density=True)
+
+
+plt.legend()
+plt.xlabel("IWV [kg m^-2]")
+plt.ylabel("frequency")
+plt.show()
 
 #%%
-#ds4 =ds.chunk(dict(time=-1))
+#selecting target day
+target_day='2024-09-07'
+target_date=target_day
+ds_sondes_f=sel_target_date(target_date,ds_sondes)
+print(ds_sondes_f.sonde_time)
+ds_hamp_f=sel_target_date(target_date,ds_hamp)
+print(ds_hamp_f.time)
+ds_halo_altitude_f=sel_target_date(target_date,ds_halo_altitude)
+print(ds_halo_altitude_f.time)
+
+df = ds_hamp_f['IWV'].to_dataframe().reset_index()
+
+
+df['time'] = pd.to_datetime(df['time'])
+df = df.sort_values('time')
+
+# Set time as the index for rolling calculation
+df.set_index('time', inplace=True)
+
+# Calculate the rolling mean with a 10-second window
+df['IWV_rolling'] = df['IWV'].rolling('10s').mean()
+
+# Reset index to revert back to a regular DataFrame
+df.reset_index(inplace=True)
+
+# Convert back to xarray DataArray
+rolling_iwv = xr.DataArray(df['IWV_rolling'].values, 
+                            coords=[df['time'].values], 
+                            dims=['time'])
+
+ds_hamp_f['IWV_rolling'] = rolling_iwv
+#%% PLOT PLOT PLOT
+# if all, instead of target day
+ds_sondes_f = ds_sondes
+ds_hamp_f = ds_hamp
+
+import matplotlib.colors as colors
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+ax.add_feature(cfeature.COASTLINE)
+
+# Get the values and locations for plotting
+lon = ds_hamp_f.lon.values  # Longitudes
+lat = ds_hamp_f.lat.values  # Latitudes
+iwv_values = ds_hamp_f.IWV.values  # IWV values #oder IWV_rolling
+
+# Normalize the IWV values for colormap
+norm = plt.Normalize(np.min([np.nanmin(iwv_values),np.nanmin(ds_sondes_f.iwv)]),np.max([np.nanmax(iwv_values),np.nanmax(ds_sondes_f.iwv)] ))
+#norm = divnorm = colors.TwoSlopeNorm(vmin=np.nanmin(iwv_values), vcenter=50, vmax=np.nanmax(iwv_values))
+#cmap= colors.LinearSegmentedColormap.from_list(    'bone', 'cool')
+colors.LogNorm(np.min([np.nanmin(iwv_values),np.nanmin(ds_sondes_f.iwv)]),np.max([np.nanmax(iwv_values),np.nanmax(ds_sondes_f.iwv)] ))
+
+cmap = plt.get_cmap('winter')  # Choose a colormap
+
+sc = ax.scatter(lon, lat, c=iwv_values, s=0.5, cmap=cmap, norm=norm, transform=ccrs.PlateCarree())
+
+#sc2 = ax.scatter(lon, lat, c=iwv_values2, s=0.5, cmap=cmap, norm=norm, transform=ccrs.PlateCarree())
+
+"""
+# Create your scatter plot with filled triangles
+sc_sondes_fill = ax.scatter(ds_sondes_f.aircraft_longitude, ds_sondes_f.aircraft_latitude,
+                             norm=norm, c=ds_sondes_f.iwv, s=30, cmap=cmap, 
+                             transform=ccrs.PlateCarree(), marker='^')  # Use triangle marker
+
+# Overlay the scatter plot with black outlines
+sc_sondes_outline = ax.scatter(ds_sondes_f.aircraft_longitude, ds_sondes_f.aircraft_latitude,
+                                c='none', edgecolor='black', s=30, 
+                                marker='^', transform=ccrs.PlateCarree())
+"""
+# Create a scatter plot with varying colors
+#sc_sondes = ax.scatter(ds_sondes_f.aircraft_longitude, ds_sondes_f.aircraft_latitude,norm=norm, edgecolor='black', c=ds_sondes_f.iwv, s=10, cmap=cmap, transform=ccrs.PlateCarree())
+
+
+
+# Add a colorbar
+cbar = plt.colorbar(sc, ax=ax, orientation='vertical',norm=norm,shrink=0.7)
+cbar.set_label('IWV [kg m-2]')
+#plt.xlim((-62,-40))
+
+#plt.ylim((3,18))
+
+plt.xlim((-62,-10))
+
+plt.ylim((0,25))
+
+#plt.title("Flight day "+target_day)
+plt.rcParams['figure.dpi'] = 400
+plt.rcParams['savefig.dpi'] = 400
+plt.show()
+#plt.savefig('/home/u/u301032/orcestra/plots/plot-iwv'+target_day+'.jpg', dpi=300)
 
 #%% Halo Ice peak analysis
 # which flights are particulary interesting? From all and the golden days
@@ -298,59 +448,175 @@ def halo_icepeak_analysis():
     plt.xlabel("T in K")
     plt.show()
 
+
 #halo_icepeak_analysis()
+#%%
+fs = fsspec.filesystem("ipns")
+#list_a=(fs.glob("ipns://latest.orcestra-campaign.org/products/HALO/position_attitude*"))
 
+ds_altitude=xr.open_dataset('ipns://latest.orcestra-campaign.org/products/HALO/position_attitude.zarr',
+                  engine="zarr")
+ds_altitude = ds_altitude.sel(time=slice('2024-08-01','2024-10-01'))
+print(ds_altitude)
 
+#%%
+#bins = np.arange(11050,15060,100)
+#(n, bins, patches) = plt.hist(ds_altitude.alt, bins)
+plt.stairs(n, bins,fill=True)
+heights =[11400.0,12650.0,13000.0,13250.0,13600.0,13850.0,14400.0,15000.0] #new
+plt.title("flight levels")
+plt.xlim(11000,15500)
+plt.vlines(heights,ymin=0,ymax=n.max(), color="red")
+plt.xlabel("height [m]")
+plt.show()
+#%%
+bins = np.arange(11025,15060,50)
+(n2, bins2, patches) = plt.hist(ds_altitude.alt, bins)
+#%%
+plt.stairs(n2, bins2,fill=True)
+heights =[11900,15000.0	,13600.0,12650.0,11400.0,13000.0,13250.0,14450.0,13900.0]#old
+heights =[11400.0,12650.0,13000.0,13250.0,13550.0,13850.0,14400.0,14950.0] #new
+#plt.legend()
+plt.title("flight levels")
+plt.xlim(11000,15500)
+plt.vlines(heights,ymin=0,ymax=n2.max(), color="red")
+plt.xlabel("height [m]")
+plt.show()
 #%% Ermitteln der Flughöhen
+# This is needed for flight level plot #
 def flight_altitudes_analysis():
-  files_altitude = 'ipns://latest.orcestra-campaign.org/products/HALO/position_attitude/*.zarr'
+  #files_altitude = 'ipns://latest.orcestra-campaign.org/products/HALO/position_attitude/*.zarr'
   #ds=xr.open_mfdataset(files_altitude,engine="zarr")
-
+  fs = fsspec.filesystem("ipns")
   ds_altitude=[]
-  list_a=(fs.glob("ipns://latest.orcestra-campaign.org/products/HALO/position_attitude/*"))
-  [ds_altitude.append(xr.open_dataset("ipns://"+i,
-                  engine="zarr"))for i in list_a]
-  ds_altitude=xr.concat([xr.open_dataset("ipns://"+i,
-                  engine="zarr")for i in list_a],dim="time")
+
+  ds_altitude = xr.open_dataset("ipfs://bafybeias3h5uxtt4ky4d4gn6l6gxjqfkzbde5jlunya6g3umnkvn7xoyoe", engine="zarr")
   ds_altitude = ds_altitude.sel(time=slice('2024-08-01','2024-10-01'))
 
-  bins = np.arange(50,15060,100)
-  (n2, bins2, patches) = plt.hist(ds_altitude.alt, bins)
-  df=pd.DataFrame({"counts":n2,"middle":bins2[:-1]+50})
-  df=df[(df["middle"]>=8000)]
+  # OTHER OPTIONS
+  #bins = np.arange(50,15060,100) test if used, use 
+  #bins = np.arange(10025,15075,50) # other options
+  #(n25_75, bins25_75, patches) = plt.hist(ds_altitude.alt, bins)#,density=True
 
-  df["rank"]=df["counts"].rank(ascending=False)
-  df.sort_values("counts", inplace = True) 
+  # FINAL SETTING
+  fig, ax = plt.subplots()
 
-  plt.stairs(n2, bins2,fill=True)
-  heights =[15000.0	,12800.0,12500.0,13600.0,12700.0,14000.0,12600.0,11400.0,13000.0,13800.0,13200.0, 14500.0,13300.0,14400.0,13900.0]
-  plt.legend()
-  plt.title("simulation levels v1")
-  plt.xlim(10000,15500)
-  plt.vlines(heights,ymin=0,ymax=15000000, color="red")
+  bins = np.arange(10050,15075,100) # final setting
+  (n50_150, bins50_150, patches) = ax.hist(ds_altitude.alt, bins,orientation='horizontal')#,color='lightseagreen')#,density=True
+
+  heights =[11400.0,12650.0,13000.0,13250.0,13600.0,13850.0,14450.0,15000.0	]
+  plt.hlines(heights,xmin=0,xmax=n50_150.max()+0.1*n50_150.max(),color='black',zorder=0)#, color="mediumslateblue") 
+  #plt.vlines(heights,ymin=0,ymax=1, color="red")
+  plt.ylabel("Height [m]")
+  plt.xlabel("Count per bin")
+
+
+  ax.spines['right'].set_visible(False)
+  ax.spines['top'].set_visible(False)
+
+  plt.rcParams['figure.dpi'] = 400
+  plt.rcParams['savefig.dpi'] = 400
+  plt.savefig('/home/u/u301032/orcestra/plots/flightlevels.png')
   plt.show()
 
-  bins = np.arange(9000,15060,30)
-  (n2, bins2, patches) = plt.hist(ds_altitude.alt, bins,log=True)#,density=True
-  heights =[11900,15000.0	,13600.0,12650.0,11400.0,13000.0,13250.0,14450.0,13900.0]
-  plt.vlines(heights,ymin=0,ymax=n2.max(), color="red")
-  plt.show()
-  plt.stairs(n2, bins2,fill=True)
-  heights =[11900,15000.0	,13600.0,12650.0,11400.0,13000.0,13250.0,14450.0,13900.0]
-  #plt.legend()
-  plt.title("flight levels")
-  plt.xlim(11000,15500)
-  plt.vlines(heights,ymin=0,ymax=n2.max(), color="red")
-  plt.xlabel("height [m]")
+
+
+  # Sorting of bins
+  #df=pd.DataFrame({"counts":n50_150,"middle":bins50_150[:-1]+50})
+  #df=df[(df["middle"]>=8000)]
+
+  #df["rank"]=df["counts"].rank(ascending=False)
+  #df.sort_values("counts", inplace = True) 
+
+
+
+
+  #plt.title("flight levels")
+
+  
   plt.show()
 
-#flight_altitudes_analysis()
+flight_altitudes_analysis()
 #%% Reading pamtra simulation
-def pamtra_halo_comparison():
-  ds_halo,ds_halo_altitude,ds_halo_iwv =Reading_halo_data()
-  file_pamtra = "/work/um0203/u301032/PAMTRA_output/PAMTRA-ICON_0829_test_factor_100_new_rh.nc"
+def pamtra_halo_BT_read_in(pamtra_file_names):
+  ds_radar,ds_halo,ds_halo_altitude,ds_halo_iwv_KW,ds_sondes=chp.reading_halo_data()
+  pamtra_files = sorted(glob.glob(pamtra_file_names))
 
-  file_pamtra2 = "/work/um0203/u301032/PAMTRA_output/PAMTRA-ICON_0829_test_factor_100.nc"
+  # open them as one concatenated multifile dataset
+  ds_pamtra = xr.open_mfdataset(
+      pamtra_files,
+      combine='nested',
+      concat_dim='grid_x')
+
+  # select "nadir looking" BTs
+  ds_pamtra = ds_pamtra.sel(angles=180,grid_y=0)
+  ds_pamtra = ds_pamtra.drop_vars(['grid_y','angles'])
+  # average over v and h polarisation
+  ds_pamtra = ds_pamtra.mean(dim='passive_polarisation')
+
+  #ds_halo no sea please
+  da_masked = ds_halo.TBs.where(ds_halo.mask_sea_land, drop=True)
+
+  # use amplifier fault mask and land sea mask
+  da_masked = da_masked.where(ds_halo.mask_amplifier_fault, drop=True)
+  # add filtered data array to variable
+  ds_halo['TBs']=  da_masked
+  excluded_frequencies = ds_halo.frequency.where(ds_halo.frequency!=184.81, drop=True)
+# Step 2: Filter the dataset to exclude the specified frequency
+  ds_halo = ds_halo.sel(frequency=excluded_frequencies)
+  return ds_halo,ds_pamtra
+
+def pamtra_halo_BT_plot(ds_halo, ds_pamtra,freq=22.24):
+  #
+  
+  # For the histograms we want data from all outlevels
+  stacked_pamtra = ds_pamtra.tb.stack(flat_dim = ['grid_x','outlevel'])
+  plt.xlabel('BTs [K]')
+  plt.hist([stacked_pamtra.sel(frequency =freq),ds_halo.sel(frequency =freq).TBs],density=True,label=["pamtra","halo"])
+  plt.legend()
+  plt.title(str(freq))
+  plt.show()
+
+
+pamtra_files='/work/um0203/u301032/PAMTRA_output/PAMTRA-ICON_0829*_025x025_2h_v1.nc'
+ds_halo, ds_pamtra =pamtra_halo_BT_read_in(pamtra_files)
+#example plot for one frequency:
+pamtra_halo_BT_plot(ds_halo, ds_pamtra,freq=22.24)
+#%%
+def plotting_multiple(filename):
+  # Creates plot of all frequenciesHstograms of BTs PAMTRA vs HAMP
+  # uses only left and bottom spines
+  # has path for saving
+  # has high resolution
+
+  # Create a figure with a 6x4 grid of subplots
+  fig, axes = plt.subplots(6, 4, sharex=True,sharey=True, figsize=(16, 24))
+  #fig, axes = plt.subplots(6, 4,figsize=(16, 24))
+  axes = axes.flatten()  # Flatten the 2D array of axes for easy iteration
+  # Stacking pamtra array in order to include all pamtra outputlevels
+  stacked_pamtra = ds_pamtra.tb.stack(flat_dim = ['grid_x','outlevel'])
+  for i, ax in enumerate(axes):
+    freq=np.array(ds_halo.frequency)[i]
+    ax.hist([stacked_pamtra.sel(frequency =freq),ds_halo.sel(frequency =freq).TBs],density=True,label=["pamtra","halo"])
+    ax.set_title(str(freq))
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+  fig.supxlabel('Brightness Temperatures [K]')
+  
+  plt.rcParams['figure.dpi'] = 400
+  plt.rcParams['savefig.dpi'] = 400
+  #plt.savefig(f'/home/u/u301032/orcestra/plots/{filename}.png')
+  plt.show()
+plotting_multiple(None)  
+#%%
+def pamtra_halo_comparison():
+  ds_radar,ds_halo,ds_halo_altitude,ds_halo_iwv_KW,ds_sondes=chp.reading_halo_data()
+  file_pamtra = "/work/um0203/u301032/PAMTRA_output/PAMTRA-ICON_0829_test_factor_100_new_rh.nc"
+  
+  # mask amplifier fault
+  HAMP_TBs =ds_halo.TBs.where(np.all(ds_halo.mask_amplifier_fault,axis=1))
+  
+  #file_pamtra2 = "/work/um0203/u301032/PAMTRA_output/PAMTRA-ICON_0829_test_factor_100.nc"
   ds_pamtra=xr.open_dataset(file_pamtra, engine="netcdf4")
 
   # select "nadir looking" BTs
@@ -361,6 +627,10 @@ def pamtra_halo_comparison():
 
   common_idx=np.load('/work/um0203/u301032/PAMTRA_output/PAMTRA-ICON_{DATE}_test_factor_100_common_idx.npy') 
   ds_pamtra= ds_pamtra.assign_coords({"grid_x":common_idx})
+
+
+
+
 
   #%% Reading ICON IWV
   path="/work/mh0492/m301067/orcestra/icon-mpim/build-lamorcestra/experiments/orcestra_1250m_0829/"
@@ -402,13 +672,13 @@ def pamtra_halo_comparison():
       return ax
 
 
-  #%% Comparison 
+  #%% Comparison FREQUENCY HALO PAMTRA
   #plotting the values:
   #ds_halo.sel(frequency =90).TBs.plot.line(x="time")
   #plt.show()
   # Calculation of bias
   #bias= np.mean(sim) - np.mean()
-
+  #def plot_BT_Halo_pamtra():
   ds_pamtra.sel(outlevel=3).tb.mean("grid_x")
   ds_halo.TBs.mean("time")
   frequencies = ds_halo.frequency
